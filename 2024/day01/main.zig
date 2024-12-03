3483005e046f6d6cb88c02c3247a180503a85acb
Sebastian Nyberg <seb.nyberg90@gmail.com>
2024-12-03 09:05:04 +0100

    Finally reading lines from a file

--- 2024/day01/main.zig
+++ 2024/day01/main.zig
@@ -1,20 +1,39 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const allocator = std.heap.page_allocator;
+
+const LineIterator = struct {
+    file: std.fs.File,
+    fbs: std.io.FixedBufferStream([]u8),
+    fn next(self: *LineIterator) ![]u8 {
+        try self.fbs.seekTo(0);
+        try self.file.reader().streamUntilDelimiter(self.fbs.writer(), '\n', self.fbs.buffer.len);
+        return self.fbs.getWritten();
+    }
+};
+
+fn lineIterator(f: std.fs.File) LineIterator {
+    var buf: [4096]u8 = undefined;
+    return LineIterator{
+        .file = f,
+        .fbs = std.io.fixedBufferStream(&buf),
+    };
+}
 
 pub fn main() void {
     const file = std.fs.cwd().openFile("./2024/day01/testinput", .{ .mode = .read_only }) catch |err| {
         std.debug.print("{any}", .{err});
         return;
     };
-    defer file.close();
-
-    var arr: [100]u8 = undefined;
-    while (file.reader().readUntilDelimiterOrEof(&arr, '\n')) |result| {
-        if (result != null) {
-            std.debug.print("{s}\n", .{result.?});
-        } else {
-            return;
-        }
+    var iter = lineIterator(file);
+    // var it = std.mem.split(u8, "abc123", ", ");
+    while (iter.next()) |line| {
+        std.debug.print("{s}\n", .{line});
     } else |err| {
-        std.debug.print("{any}", .{err});
+        switch (err) {
+            error.EndOfStream => return,
+            else => std.debug.print("Error reading line {any}\n", .{err}),
+        }
     }
+    defer file.close();
 }
